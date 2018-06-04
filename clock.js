bf267f77b10cf43e7a377b3843f0a1a8d836334d
Jacob Byers <byers.jacob@gmail.com>
2018-06-04 14:02:57 -0700

    Adding days variable

--- clock.js
+++ clock.js
@@ -2,4 +2,5 @@ var d = new Date();
 var h = d.getHours();
 var m = d.getMinutes();
 var s = d.getSeconds();
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 console.log(h + ":" + m + ":" + s);
