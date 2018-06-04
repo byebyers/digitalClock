3bc4a7ff90bd57fd4fdff38c3996fed0cc228a92
Jacob Byers <byers.jacob@gmail.com>
2018-06-04 14:05:02 -0700

    Adding .getday();

--- clock.js
+++ clock.js
@@ -2,5 +2,6 @@ var d = new Date();
 var h = d.getHours();
 var m = d.getMinutes();
 var s = d.getSeconds();
+var day = d.getDay();
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 console.log(h + ":" + m + ":" + s);
