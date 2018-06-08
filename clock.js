ddfcf5320435ac4aa9debc765617b18d20d1f0f7
Jacob Byers <byers.jacob@gmail.com>
2018-06-08 13:19:42 -0700

    Added var dom to get date of the month.

--- clock.js
+++ clock.js
@@ -3,6 +3,7 @@ var h = d.getHours();
 var m = d.getMinutes();
 var s = d.getSeconds();
 var day = d.getDay();
+var dom = d.getDate();
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 var month = d.getMonth();
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
@@ -18,5 +19,6 @@ function ampm(c) {
   }
 }
 
+console.log(dom);
 console.log(ampm(h));
 console.log(months[month] + " - " + days[day] + " - " + h + ":" + m + " " + ampm(h));
