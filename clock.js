ccac89b92a4c74cda0fbaae5eb4a97d3bde40bb3
Jacob Byers <byers.jacob@gmail.com>
2018-06-09 10:20:45 -0700

    Adding notes for global variables.

--- clock.js
+++ clock.js
@@ -2,10 +2,10 @@ var d = new Date();
 var h = d.getHours();
 var m = d.getMinutes();
 var s = d.getSeconds();
-var day = d.getDay();
-var dom = d.getDate();
+var day = d.getDay();  //Get day number of the week
+var dom = d.getDate(); //Get day number of the month
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var month = d.getMonth();
+var month = d.getMonth(); //Get month number of the year
 var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 
