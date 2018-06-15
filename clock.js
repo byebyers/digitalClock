431729b2482913a6ce1b819aee5236197a772714
Jacob Byers <byers.jacob@gmail.com>
2018-06-15 10:34:10 -0700

    added emphasis in dom + st, nd, rd, or th.

--- clock.js
+++ clock.js
@@ -1,22 +1,22 @@
-var d = new Date();
-var h = d.getHours();
-var m = d.getMinutes();
-var s = d.getSeconds();
-var day = d.getDay();  //Get day number of the week
-var dom = d.getDate(); //Get day number of the month
-var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var month = d.getMonth(); //Get month number of the year
-var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 
 function startTime() {
     var today = new Date();
     var h = today.getHours();
     var m = today.getMinutes();
     var s = today.getSeconds();
+    var dow = today.getDay();  //Get day number of the week
+    var dom = today.getDate(); //Get day number of the month
+    var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var month = today.getMonth(); //Get month number of the year
+    var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     m = checkTime(m);
     s = checkTime(s);
+    me = ampm(h);
+    civ = civil(h);
+    em = emph(dom);
     document.getElementById('clock').innerHTML =
-    h + ":" + m + ":" + s;
+    days[dow] + ", " + months[month] + " " + em + ", " + civ + ":" + m + ":" + s + " " + me;
     var t = setTimeout(startTime, 500);
 }
 function checkTime(i) {
@@ -24,6 +24,42 @@ function checkTime(i) {
     return i;
 }
 
+//Get am/pm
+function ampm(c) {
+  if (c <= 12) {
+    return "AM";
+  }
+  else {
+    return "PM";
+  }
+}
+
+//Get Civilian time
+function civil(t) {
+  if (t <= 12) {
+    return t;
+  }
+  else {
+    return t - 12;
+  }
+}
+
+function emph(t) {
+  switch (t) {
+    case 1:
+    case 31:
+      return t + "st";
+      break;
+    case 2:
+      return t + "nd";
+      break;
+    case 3:
+      return t + "rd";
+      break;
+    default:
+      return t + "th";
+  }
+}
 /*!
 function insertClock()    {
       var time = months[month] + " " + dom + " - " + days[day] + " - " + civil(h) + ":" + doubled(m) + ":" + doubled(s) + " " + ampm(h);
