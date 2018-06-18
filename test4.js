85128e660c75ded3f6604a814be27ff47c3c20f8
Jacob Byers <byers.jacob@gmail.com>
2018-06-18 15:20:43 -0700

    test

---
+++ test4.js
@@ -0,0 +1,19 @@
+// Setup
+var myObj = {
+  gift: "pony",
+  pet: "kitten",
+  bed: "sleigh"
+};
+
+function checkObj(checkProp) {
+  // Your Code Here
+  if (myObj.hasOwnProperty(checkProp)) {
+    console.log(myObj[checkProp]);
+  }
+  console.log("Not Found");
+}
+
+// Test your code by modifying these values
+checkObj("gift");
+
+console.log(myObj.gift);
